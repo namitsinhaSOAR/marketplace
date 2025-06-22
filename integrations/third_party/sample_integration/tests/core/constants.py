d85de06892a4670637e7755059dea3e54de5c70a
Roi Shemesh <rois@google.com>
2025-06-22 22:43:46 +0300

    added tests core files

---
+++ integrations/third_party/sample_integration/tests/core/constants.py
@@ -0,0 +1,3 @@
+KNOWN_SYMBOLS = ['EUR', 'USD', 'JPY', 'BGN', 'CZK', 'DKK', 'GBP', 'HUF', 'PLN', 'RON', 'SEK', 'CHF', 'ISK', 'NOK',
+                 'TRY', 'AUD', 'BRL', 'CAD', 'CNY', 'HKD', 'IDR', 'ILS', 'INR', 'KRW', 'MXN', 'MYR', 'NZD', 'PHP',
+                 'SGD', 'THB', 'ZAR']
---
+++ integrations/third_party/sample_integration/tests/core/session.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+
+import datetime
+from typing import Iterable
+
+from integration_testing import router
+from integration_testing.common import get_request_payload
+from integration_testing.request import MockRequest
+from integration_testing.requests.response import MockResponse
+from integration_testing.requests.session import MockSession, RouteFunction, Response
+from TIPCommon.types import SingleJson
+
+from .product import VatComply
+
+
+class VatComplySession(MockSession[MockRequest, MockResponse, VatComply]):
+    def get_routed_functions(self) -> Iterable[RouteFunction[Response]]:
+        return [
+            self.get_rates,
+        ]
+
+    @router.get(r"/rates")
+    def get_rates(self, request: MockRequest) -> MockResponse:
+        try:
+            params: SingleJson = get_request_payload(request)
+            base: str | None = params.get("base")
+            symbols: str | None = params.get("symbols")
+            date: str | None = params.get("date")
+            rates = self._product.get_rates(base, symbols, date)
+            return MockResponse(content=rates)
+        except ValueError as e:
+            return MockResponse(content=str(e), status_code=422)
+
